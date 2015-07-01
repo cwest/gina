0e3faee316f464b7ff7829b98f94a012967da116
Casey West <casey@hobbit.home>
2015-06-30 22:39:11 -0400

    Initial Gina.

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source "https://rubygems.org"
+
+ruby "2.2.0"
+
+gem "lita"
+
+# Cloud Foundry
+gem "cf-app-utils"
+
+# Adapters
+gem "lita-slack"
+
+# Handlers
+gem "lita-applause"
+gem "lita-ascii-art"
+gem "lita-baby_elephant"
+gem "lita-catgif"
+gem "lita-dice"
+gem "lita-eightball"
+gem "lita-google-images"
+gem "lita-karma"
---
+++ lita_config.rb
@@ -0,0 +1,34 @@
+Lita.configure do |config|
+  # The name your robot will use.
+  config.robot.name = "gina"
+
+  config.http.port = ENV["PORT"]
+
+  # The locale code for the language to use.
+  # config.robot.locale = :en
+
+  # The severity of messages to log. Options are:
+  # :debug, :info, :warn, :error, :fatal
+  # Messages at the selected level and above will be logged.
+  config.robot.log_level = :info
+
+  # An array of user IDs that are considered administrators. These users
+  # the ability to add and remove other users from authorization groups.
+  # What is considered a user ID will change depending on which adapter you use.
+  config.robot.admins = ENV["GINA_ADMINS"].split(/,/)
+
+  # The adapter you want to connect with. Make sure you've added the
+  # appropriate gem to the Gemfile.
+  config.robot.adapter = :slack
+  config.adapters.slack.token = ENV["GINA_SLACK_TOKEN"]
+
+  ## Example: Set options for the Redis connection.
+  redis = CF::App::Credentials.find_all_by_all_service_tags(['redis', 'pivotal']).first
+  config.redis["host"]     = redis.fetch('host')
+  config.redis["port"]     = redis.fetch('port')
+  config.redis["password"] = redis.fetch('password')
+
+  ## Example: Set configuration for any loaded handlers. See the handler's
+  ## documentation for options.
+  # config.handlers.some_handler.some_config_key = "value"
+end
