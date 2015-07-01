f6eda7d111e8b0311da3f6da46d4f796ca836ff8
Casey West <casey@hobbit.home>
2015-06-30 22:49:06 -0400

    Remove cooldown period for karma.

--- lita_config.rb
+++ lita_config.rb
@@ -28,7 +28,5 @@ Lita.configure do |config|
   config.redis["port"]     = redis.fetch('port')
   config.redis["password"] = redis.fetch('password')
 
-  ## Example: Set configuration for any loaded handlers. See the handler's
-  ## documentation for options.
-  # config.handlers.some_handler.some_config_key = "value"
+  config.handlers.karma.cooldown = nil
 end
